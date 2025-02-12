53d7e37420db53ca7a7aabc069af95a0f89e9a11
himanshu-45naik <himanshugit45@gmail.com>
2025-02-12 12:59:15 +0530

    Updated data ingestion step

---
+++ steps/data_ingestion_step.py
@@ -0,0 +1,16 @@
+from src.ingest_data import DataIngestorFactory
+from zenml import step
+import pandas as pd
+
+@step
+def data_ingestion_step(file_path:str)->pd.DataFrame:
+    """Ingest data from a zip file using the appropriate Data ingester
+
+    Args:
+        file_path (str): the path of file where the zip file exists
+    """
+    data_ingestor = DataIngestorFactory.get_data_ingestor(file_path)
+    
+    df = data_ingestor.ingest_df(file_path)
+
+    return df
