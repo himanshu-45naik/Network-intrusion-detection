9509f1f69c28a7ad556751e0a1c7f85648c6e23c
himanshu-45naik <himanshugit45@gmail.com>
2025-02-20 00:12:33 +0530

    Handling duplicate values

---
+++ steps/handle_duplicate_step.py
@@ -0,0 +1,18 @@
+from src.handle_duplicate_values import Handler
+import pandas as pd
+from zenml import step
+
+@step
+def drop_duplicate_values(df:pd.DataFrame)->pd.DataFrame:
+    """Drops duplicate values from the dataframe.
+
+    Args:
+        df (pd.DataFrame): The dataframe to perform transformation.
+        
+    Return:
+        pd.DataFrame : Transformed dataframe.
+    """
+    handle_data = Handler()
+    df_cleaned = handle_data.handle_duplicate_values(df)
+    
+    return df_cleaned
