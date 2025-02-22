d9b9e8cd99c27acf46a11936523007e07aa88bc9
himanshu-45naik <himanshugit45@gmail.com>
2025-02-22 17:52:55 +0530

    ML Pipeline

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -4,6 +4,9 @@ import numpy as np
 from steps.data_ingestion_step import data_ingestion
 from steps.handle_duplicate_step import drop_duplicate_values
 from steps.data_handling_step import handle_missing_values, handle_infinite_values
+from steps.downcast_step import downcast
+from steps.feature_engineering_step import feature_engineering
+from steps.feature_extraction_step import feature_extraction
 
 
 @pipeline(model=Model(name="intrusion_predictor"))
@@ -27,10 +30,20 @@ def ml_pipeline():
     missing_features = [f for f in numeric_df.columns if np.isnan(numeric_df[f]).any()]
     filled_df = handle_missing_values(inf_replaced_data, missing_features, "median")
     
-    ## Downcast
+    ## Downcasting step.
+    downcasted_df = downcast(filled_df)
     
-    ## Scaling
+    ## Dropping feature step.
+    modified_df = feature_engineering(downcasted_df,"dropfeatures",features=None)
     
-    ## PCA
+    ## Scaling step.
+    features_df = modified_df.drop('Attack type',axis = 1)
+    attacks_df = modified_df['Attack type']
+    features = modified_df.columns
+    scaled_df = feature_engineering(features_df,"standard",features)
     
-    ## SMOTE
+    ## PCA step.
+    extracted_df = feature_extraction(scaled_df)
+    extracted_df["Attack Type"] = attacks_df.values
+    
+    ## SMOTE.
