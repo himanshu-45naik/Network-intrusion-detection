28cec0bfa5ea606dc17616fb3259b33c6678e98a
himanshu-45naik <himanshugit45@gmail.com>
2025-03-01 19:27:08 +0530

    Updated data handling

--- src/handle_duplicate_values.py
+++ src/handle_duplicate_values.py
@@ -4,32 +4,33 @@ from abc import ABC, abstractmethod
 
 # Configure logging
 logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s"
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
+
 
 class DuplicateValuesHandling(ABC):
     @abstractmethod
     def transform(self, df: pd.DataFrame) -> pd.DataFrame:
         """
         Performs transformation on the dataframe.
-        
+
         Args:
             df (pd.DataFrame): Input DataFrame
-            
+
         Returns:
             pd.DataFrame: Transformed DataFrame
         """
         pass
 
+
 class DropDuplicateValues(DuplicateValuesHandling):
     def transform(self, df: pd.DataFrame) -> pd.DataFrame:
         """
         Drops the duplicate values.
-        
+
         Args:
             df (pd.DataFrame): Input DataFrame
-            
+
         Returns:
             pd.DataFrame: DataFrame with duplicates removed
         """
@@ -38,23 +39,23 @@ class DropDuplicateValues(DuplicateValuesHandling):
         logging.info("Successfully dropped duplicate values.")
         return df_cleaned
 
+
 class Handler:
     @staticmethod
     def handle_duplicate_values(df: pd.DataFrame) -> pd.DataFrame:
         """
         Executes the transformation.
-        
+
         Args:
             df (pd.DataFrame): Input DataFrame
-            
+
         Returns:
             pd.DataFrame: Transformed DataFrame
         """
         handler = DropDuplicateValues()
-        transformed_df =  handler.transform(df)
+        transformed_df = handler.transform(df)
         return transformed_df
 
-    
-if __name__ == '__main__':
-    pass        
-    
+
+if __name__ == "__main__":
+    pass
--- steps/handle_duplicate_step.py
+++ steps/handle_duplicate_step.py
@@ -2,17 +2,18 @@ from src.handle_duplicate_values import Handler
 import pandas as pd
 from zenml import step
 
+
 @step
-def drop_duplicate_values(df:pd.DataFrame)->pd.DataFrame:
+def drop_duplicate_values(df: pd.DataFrame) -> pd.DataFrame:
     """Drops duplicate values from the dataframe.
 
     Args:
         df (pd.DataFrame): The dataframe to perform transformation.
-        
+
     Return:
         pd.DataFrame : Transformed dataframe.
     """
     handle_data = Handler()
     df_cleaned = handle_data.handle_duplicate_values(df)
-    
+
     return df_cleaned
