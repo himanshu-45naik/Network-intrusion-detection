a0ffba4851c41a8edca8a8808353da3d11a96ca3
himanshu-45naik <himanshugit45@gmail.com>
2025-03-01 19:23:52 +0530

    ML pipeline

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -3,10 +3,16 @@ import pandas as pd
 import numpy as np
 from steps.data_ingestion_step import data_ingestion
 from steps.handle_duplicate_step import drop_duplicate_values
-from steps.data_handling_step import handle_missing_values, handle_infinite_values
+from steps.data_handling_step import (
+    handle_missing_data,
+    handle_infinite_values,
+    feature_name_handling,
+)
 from steps.downcast_step import downcast
 from steps.feature_engineering_step import feature_engineering
 from steps.feature_extraction_step import feature_extraction
+from steps.data_splitting_step import data_splitter_step
+from steps.oversampling_data_step import sampling_data
 
 
 @pipeline(model=Model(name="intrusion_predictor"))
@@ -14,36 +20,43 @@ def ml_pipeline():
     """Defines end-to-end machine learning pipeline."""
 
     ## Data Ingestion step.
-    raw_df = data_ingestion("/home/himanshu/Coding/Network Intrusion/archive.zip")
+    raw_df = data_ingestion("/home/himanshu/Coding/Network Intrusion/cicids17.zip")
 
+    ## Updating Feature name step
+    updated_raw_df = feature_name_handling(raw_df)
     ## Drop duplicate values step.
-    transformed_df = drop_duplicate_values(raw_df)
+    transformed_df = drop_duplicate_values(updated_raw_df)
 
     ## Replacing infinity values with Nan step.
-    numeric_df = transformed_df.select_dtypes(["number"])
-    inf_features = [
-        col for col in numeric_df.columns if np.isinf(transformed_df[col]).any()
-    ]
-    inf_replaced_data = handle_infinite_values(transformed_df, inf_features)
+    inf_replaced_data = handle_infinite_values(transformed_df)
 
     ## Filling missing values step.
-    missing_features = [f for f in numeric_df.columns if np.isnan(numeric_df[f]).any()]
-    filled_df = handle_missing_values(inf_replaced_data, missing_features, "median")
-    
+
+    filled_df = handle_missing_data(inf_replaced_data, "median")
+
     ## Downcasting step.
     downcasted_df = downcast(filled_df)
-    
+
     ## Dropping feature step.
-    modified_df = feature_engineering(downcasted_df,"dropfeatures",features=None)
-    
+    modified_df = feature_engineering(downcasted_df, "dropfeatures")
+
     ## Scaling step.
-    features_df = modified_df.drop('Attack type',axis = 1)
-    attacks_df = modified_df['Attack type']
-    features = modified_df.columns
-    scaled_df = feature_engineering(features_df,"standard",features)
-    
+    scaled_df = feature_engineering(
+        modified_df, "standard"
+    )  ## This do not contain the attack type feature
+
     ## PCA step.
     extracted_df = feature_extraction(scaled_df)
-    extracted_df["Attack Type"] = attacks_df.values
     
-    ## SMOTE.
+    ## Label encoding for binary classification step
+    encoded_df = feature_engineering(extracted_df, "binaryencoding")
+    
+    ## Data splitting step.
+    X_train, X_test, y_train, y_test = data_splitter_step(encoded_df, "Attack Type")
+    
+    ## SMOTE step.
+    X_train_resampled, y_train_resampled = sampling_data(X_train, y_train)
+
+
+if __name__ == "__main__":
+    pass
